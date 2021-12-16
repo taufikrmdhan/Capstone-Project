3b53cb75dfac3f79f4323378358f2bd182bb6941
taufikrmdhan <59110317+taufikrmdhan@users.noreply.github.com>
2021-12-16 15:18:42 +0700

    revisi design frontend

---
+++ Website - server- client/frontend/src/components/Sistem.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from 'react'
+import axios from "axios";
+
+const Sistem = () => {
+    const [products, setProduct] = useState([]);
+
+    useEffect(() => {
+        getProduct();
+    }, []);
+
+    const getProduct = async () => {
+        const response = await axios.get('http://localhost:5000/products');
+        setProduct(response.data);
+    }
+
+    return (
+<div className="container">
+<p class="title_daftar">Daftar Ikan Channa</p>
+
+<div class="list_sensor">
+    <p>
+      list log sensor pH
+    </p>
+    <ul>
+      <li>23/05/2021 -jam 08:00</li>
+      <p>
+        5 pH
+      </p>
+      <li>23/05/2021 -jam 08:10</li>
+      <p>
+        6 pH
+      </p>
+      <li>23/05/2021 -jam 08:20</li>
+      <p>
+        4 pH
+      </p>
+    </ul>
+  </div>
+</div>
+    )
+}
+
+export default Sistem
