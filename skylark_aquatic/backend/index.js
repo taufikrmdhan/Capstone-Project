19dc1bd81ed0c95d9646dfaceeaadf53674342ad
Taufik Ramadhan <taufikramadhan301120@gmail.com>
2021-12-20 21:27:55 +0700

    nambah limit db

--- skylark_aquatic/backend/index.js
+++ skylark_aquatic/backend/index.js
@@ -14,10 +14,10 @@ try {
 }
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit:'1024mb'}));
 app.use('/products', productRoutes);
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit:'1024mb'}));
 app.use('/dht11', dhtRoutes);
 
 app.listen(5000, () => console.log('Server running at port 5000'));
